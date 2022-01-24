3be68525e345a93af5053e49a0e4e34cb7614cef
Alexey Rochev <equeim@gmail.com>
2022-01-25 01:52:58 +0300

    qt5-tools: disable building of unneeded tools

--- ports/qt5-tools/portfile.cmake
+++ ports/qt5-tools/portfile.cmake
@@ -2,8 +2,13 @@ include(${CURRENT_INSTALLED_DIR}/share/qt5/qt_port_functions.cmake)
 
 qt_submodule_installation(PATCHES
     icudt-debug-suffix.patch # https://bugreports.qt.io/browse/QTBUG-87677
+    disable-tools.patch
 )
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
 if(EXISTS "${CURRENT_INSTALLED_DIR}/plugins/platforms/qminimal${VCPKG_TARGET_SHARED_LIBRARY_SUFFIX}")
     file(INSTALL "${CURRENT_INSTALLED_DIR}/plugins/platforms/qminimal${VCPKG_TARGET_SHARED_LIBRARY_SUFFIX}" DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}/bin/plugins/platforms")
 endif()
+
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
