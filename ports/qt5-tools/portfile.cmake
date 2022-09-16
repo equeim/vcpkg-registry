3933eb72178f6fdcfd863196a0304f6864299512
Alexey Rochev <equeim@gmail.com>
2022-09-16 23:21:43 +0300

    Update qt5-tools to 5.15.6

--- ports/qt5-tools/portfile.cmake
+++ ports/qt5-tools/portfile.cmake
@@ -1,7 +1,6 @@
 include(${CURRENT_INSTALLED_DIR}/share/qt5/qt_port_functions.cmake)
 
 qt_submodule_installation(PATCHES
-    icudt-debug-suffix.patch # https://bugreports.qt.io/browse/QTBUG-87677
     disable-tools.patch
 )
 
